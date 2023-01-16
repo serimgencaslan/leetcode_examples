015108a2a5ca20c021fa07624a664c655755c8fd
Serim Gençaslan <serimgencaslan49@gmail.com>
2023-01-17 00:21:06 +0300

    217 contains duplicate soluitons added

---
+++ 217-ContainsDuplicate/contains_duplicate.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+    
+class Solution2:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
